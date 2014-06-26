d2b00907c741cb63d077a48598d40db661101e0f
tampham47 <tampham47@live.com>
2014-06-26 14:31:34 +0700

    Make it more flexibility. - changed images. - assign elements for variables.

--- sutrix/sutrix.js
+++ sutrix/sutrix.js
@@ -1,100 +1,102 @@
 //sutrix carousel
 
-// not center
-
 (function ($) {
-	var option =
+	var opt =
 	{
-		available: true
+		//carousel on or off.
+		available: true,
+		//flag: if is_moving on (is_moving=true), your click is not available.
+		is_moving: false
 	}
 
 	$.fn.sutrix = function() {
-		// assign for access everywhere.
-		option.root = this;
+		// assign to easy access everywhere.
+		opt.root = this;
+		opt.viewport = $(".viewport", this);
+		opt.controls = $(".controls", this);
+		opt.holder = $(".holder", opt.viewport);
 
 		// detect window resize
 		$.screen_change();
 		$(window).resize($.screen_change);
 
-		// $("#uull").ready(function(){
-		// 	console.log("uull ready");
-		// 	var a = $("#sutrix").width();
-		// 	var b = $("#uull").width();
-
-		// 	// $("#uull").css("margin-left", "-=" + (b/2 - a/2) + "px");
-		// 	// $("#uull").css("margin-left", "-=900px");
-		// 	// $("#uull").css("margin-left", "+=532px");
-		// })
-
 		// fire an event when click on an image
-		$(".viewport img").bind("click", $.img_click);
+		$("img", opt.viewport).bind("click", $.img_click);
 
-		// move to left
-		$(".controls .left").bind("click", function(){
-			$.move_left();
-		});
-
-		//move to right
-		$(".controls .right").bind("click", function(){
-			$.move_right();
-		});
+		// move left & right
+		$(".left", opt.controls).bind("click", $.move_left);
+		$(".right", opt.controls).bind("click", $.move_right);
 	}
 
 	$.screen_change = function() {
-		// var root = $("#sutrix");
-		var root = option.root;
-		var uull = $("#uull");
-
-		if (uull.width() < root.width())
+		if (opt.holder.width() < opt.root.width())
 		{
-			$("#sutrix .controls").hide(500);
-			option.available = false;
+			opt.available = false;
+			opt.controls.hide(500);
 		} else
 		{
-			$("#sutrix .controls").show(500);
-			option.available = true;
+			opt.available = true;
+			opt.controls.show(500);
 		}
 	}
 
 	$.img_click = function() {
 		var img = $(this);
-
-		var mid_root = option.root.width() / 2;
+		var mid_root = opt.root.width() / 2;
 		var mid_element = img.offset().left + img.width() / 2;
-		var delta = Math.abs(mid_root - mid_element);
 
 		//move to center
 		if (mid_root - mid_element > 0)
-		{
-			$.move_right(delta);
-		} else
-		{
-			$.move_left(delta);
-		}
+			$.move_right();
+		else
+			$.move_left();
 	}
 
-	$.move_left = function(delta) {
-		if (!option.available) return;
+	$.move_left = function() {
+		if (!opt.available || opt.is_moving) return;
 
-		delta = 532;
-		// if (delta === undefined) delta = 532;
+		// turn flag on
+		opt.is_moving = true;
 
-		$("#uull").animate({left: "-="+ delta +"px"}, 800, function()
+		// get width of the last one
+		var holder = opt.holder;
+		var item = holder.find("li:first");
+		var delta = item.width();
+
+		holder.animate({left: "-="+ delta +"px"}, 800, function()
 		{
-			$("#uull").css("margin-left", "+="+ delta +"px");
-			$("#uull").find("li:last").after($("#uull").find("li").slice(0,1));
+			holder.css("margin-left", "+="+ delta +"px");
+			holder.find("li:last").after(holder.find("li").slice(0,1));
+
+			holder.css("margin-left", "0px");
+			holder.css("left", "0px");
+
+			// turn flag off
+			opt.is_moving = false;
 		});
 	}
 
-	$.move_right = function(delta) {
-		if (!option.available) return;
+	$.move_right = function() {
+		if (!opt.available || opt.is_moving) return;
 
-		delta = 532;
-		// if (delta === undefined) delta = 532;
+		// turn flag on
+		opt.is_moving = true;
 
-		$("#uull").css("margin-left", "-="+ delta +"px");
-		$("#uull").animate({left: "+="+ delta +"px"}, 800);
-		$("#uull").find("li:first").before($("#uull").find("li").slice(-1));
+		// get width of the first one
+		var holder = opt.holder;
+		var item = holder.find("li:last");
+		var delta = item.width();
+
+		holder.css("margin-left", "-="+ delta +"px");
+		holder.find("li:first").before(holder.find("li").slice(-1));
+
+		holder.animate({left: "+="+ delta +"px"}, 800, function(){
+			holder.css("margin-left", "0px");
+			holder.css("left", "0px");
+
+			// turn flag off
+			opt.is_moving = false;
+		});
 	}
 
 }(jQuery));
